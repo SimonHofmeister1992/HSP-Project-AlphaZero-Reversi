94f5fb068ef5541c9722ba56bc92491135ece62a
Simon Hofmeister <simon.hofmeister@st.oth-regensburg.de>
2019-11-27 00:02:43 +0100

    implemented classes for cli options

---
+++ ReversiXT_Client/ReversiAlphaGo/src/main/java/de/othr/reversixt/ReversiAlphaGo/general/CLI.java
@@ -0,0 +1,42 @@
+package de.othr.reversixt.ReversiAlphaGo.general;
+
+import org.apache.commons.cli.*;
+
+public class CLI {
+
+    private Options options;
+    private CommandLine cmd;
+
+    public CLI (String[] args) {
+        defineOptions();
+        parseCommandLine(args);
+    }
+
+    private void defineOptions() {
+        this.options = new Options();
+        options.addOption("g", ICLIOptions.GROUP_NUMBER, true, "the group number the ai will use to identify itself");
+        options.addOption("q", ICLIOptions.QUIET_MODE, false, "suppresses output of the application to a minimum");
+        options.addOption("a", ICLIOptions.IP_ADDRESS, true, "ip address of server");
+        options.addOption("p", ICLIOptions.PORT, true, "port of server");
+    }
+
+    private void parseCommandLine(String[] args) {
+        CommandLineParser clp = new DefaultParser();
+        try{
+            cmd =  clp.parse(options, args);
+        }
+        catch(ParseException e) {
+            cmd = null;
+            System.err.println("Commandline arguments could not be parsed correctly");
+        }
+    }
+
+    public boolean hasOption(String option){
+        return cmd.hasOption(option);
+    }
+    public String getOptionValue(String option){
+        return cmd.getOptionValue(option);
+    }
+
+}
+
--- ReversiXT_Client/ReversiAlphaGo/src/main/java/de/othr/reversixt/ReversiAlphaGo/general/Main.java
+++ ReversiXT_Client/ReversiAlphaGo/src/main/java/de/othr/reversixt/ReversiAlphaGo/general/Main.java
@@ -7,9 +7,24 @@ package de.othr.reversixt.ReversiAlphaGo.general;
 public class Main 
 {
     public static boolean QUIET_MODE = Boolean.FALSE;
+    private static String ip = "127.0.0.1"; // localhost
+    private static int port = 7777;
+    private static int groupNumber = 1;
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /**********************************
+         *         Commandline options
+         */
+        CLI cli = new CLI(args);
+        setValuesFromCLI(cli);
     }
+
+    private static void setValuesFromCLI(CLI cli){
+        if(cli.hasOption(ICLIOptions.GROUP_NUMBER)) groupNumber =  Integer.parseInt(cli.getOptionValue(ICLIOptions.GROUP_NUMBER));
+        if(cli.hasOption(ICLIOptions.QUIET_MODE)) QUIET_MODE = Boolean.TRUE;
+        if(cli.hasOption(ICLIOptions.IP_ADDRESS)) ip = cli.getOptionValue(ICLIOptions.IP_ADDRESS);
+        if(cli.hasOption(ICLIOptions.PORT)) port = Integer.parseInt(cli.getOptionValue(ICLIOptions.PORT));
+    }
+
 }
