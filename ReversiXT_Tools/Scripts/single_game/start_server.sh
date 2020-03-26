0c8ea3c8e6f2879ac6af2451ae36a69dd290b754
hos47096 <simon1.hofmeister@st.oth-regensburg.de>
2020-03-26 16:53:47 +0100

    added untared linux files, adjusted multigame script

--- ReversiXT_Tools/Scripts/single_game/start_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-read -p "Map: " map
-#nohup ../ServerNoGL/LinuxVersion_ServerNOGL/server_nogl -m ../Maps/actualLearningMaps/lmap1_2p.map > logs/server.out
-nohup ../ServerNoGL/LinuxVersion_ServerNOGL/server_nogl -m ../Maps/actualLearningMaps/${map} > logs/server.out
